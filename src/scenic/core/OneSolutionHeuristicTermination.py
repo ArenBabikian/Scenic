ac72ae902d84ee2ed502762e779de8feca8e780d
Aren Babikian <aren.babikian@mail.mcgill.ca>
2022-03-09 14:35:49 -0500

    add logging for partial solution

--- src/scenic/core/OneSolutionHeuristicTermination.py
+++ src/scenic/core/OneSolutionHeuristicTermination.py
@@ -25,6 +25,30 @@ class OneSolutionHeuristicTermination(Termination):
                     break
             if sol_is_valid:
                 valid_sols.append(sol)
+        
+        # # PRINT BEST HEURISTIC VAL
+        # mostZeros = -1
+        # bestSol = None
+        # for sol in F:
+        #     # for each fitness result collection
+        #     # print(f'{i} = {tuple(sol)}')
+        #     i+=1
+        #     numZeros = 0
+        #     for heu_i in range(len(sol)):
+        #         heu_v = sol[heu_i]
+        #         if heu_v == 0:
+        #             numZeros += 1
+        #     if numZeros > mostZeros:
+        #         bestSol = sol
+        #         mostZeros = numZeros
+                
+        #     if numZeros == mostZeros:
+        #         bestSum = sum(bestSol)
+        #         curSum = sum(sol)
+        #         if curSum < bestSum:
+        #             bestSol = sol
+
+        # print(bestSol)
 
         # print(valid_sols)
         return len(valid_sols) == 0
