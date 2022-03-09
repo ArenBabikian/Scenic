9babc1f845fe5cb2f88d24118b8e4c1d06c8ddd4
Aren Babikian <aren.babikian@mail.mcgill.ca>
2022-03-09 13:55:52 -0500

    Adjust and add scripts for running scenic

--- runScenic.sh
+++ runScenic.sh
@@ -7,15 +7,16 @@
 
 cmd="poetry run scenic \
 -b --count 1 -v 3 \
+-p timeout 60 \
 -p nsga False \
 -p nsga-Iters 500 \
 -p nsga-NumSols 3 \
 -p no-validation False \
--p outputWS out-nsga \
+-p outputWS out-oszkar \
 -p outputDir _ \
 -p saveImgs False \
 -p viewImgs True \
 -p saveFiles False \
--p map maps/zalaFullCrop.xodr
-../config/run.scenic"
+-p map maps/Town02.xodr
+config/figForOszkar.scenic"
 eval $cmd
