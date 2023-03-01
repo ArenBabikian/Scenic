62ad2addff74d2bfd4e486a60df13abda5784ad8
ArenBabikian <aren.babikian@mail.mcgill.ca>
2023-03-01 04:51:07 -0500

    Add meas setup for partial scenes

--- src/scenic/runmeasurements.py
+++ src/scenic/runmeasurements.py
@@ -3,23 +3,32 @@ import subprocess
 import sys
 import os
 
-short=False
+setting = "short" # short, scale, constraints
 
-if short:
+if setting == "short":
     maps = ['tram05', 'town02', 'zalaFullcrop']
     configurations = ['2actors', '3actors', '4actors']
-    scene_ids = range(10) # TODO 10
+    scene_ids = range(10)
     approaches = ['sc1', 'sc2', 'sc3', 'nsga']
     num_iterations = 10
     timeout = [600, 600, 600, 600] # 10min
-else:
-    #scale
+elif setting == "scale":
     maps = ['zalaFullcrop']
     configurations = ['7actors'] # 4 5 6 7
     scene_ids = range(5)
     approaches = ['nsga']
     num_iterations = 5
     timeout = [7200] # 2h
+elif setting == "constraints":
+    maps = ['zalaFullcrop']
+    # configurations = ['cons/none', 'cons/r', 'cons/rc', 'cons/rcv', 'cons/rcvd'] # TODO add 'cons/rcvdp'
+    configurations = ['cons/rcp', 'cons/rcpd', 'cons/rcpdv']
+    scene_ids = range(10)
+    approaches = ['nsga']
+    num_iterations = 10
+    timeout = [600] # 10min
+else:
+    exit()
 
 verbosity = 0
 save = True
@@ -29,7 +38,14 @@ for config in configurations:
         for i in scene_ids:
             for a_ind in range(len(approaches)):
                 approach = approaches[a_ind]
-                pathToFile = f'{m}/{config}/{i}-0/d-{approach}'
+                if setting != "constraints":
+                    pathToFile = f'{m}/{config}/{i}-0/d-{approach}'
+                else:
+                    pathToFile = f'{m}/{config}/{i}'
+                fullPathToFile = f'measurements/data/{pathToFile}.scenic'
+                if not os.path.exists(fullPathToFile):
+                    print(f'File not found at: {fullPathToFile}')
+                    continue
                 command = ['scenic', '-b']
                 command.extend(['--count', str(num_iterations)])
                 command.extend(['-v', str(verbosity)])
@@ -43,8 +59,8 @@ for config in configurations:
                 command.extend(['-p', 'saveFiles', str(save)])
                 command.extend(['-p', 'saveStats', str(save)])
                 command.extend(['-p', 'restart-time', str(-1)])
-                command.append(f'measurements/data/{pathToFile}.scenic')
-                print(pathToFile)
+                command.append(fullPathToFile)
+                print(fullPathToFile)
 
                 # p = subprocess.Popen(command, stderr=sys.stderr, stdout=sys.stdout, shell=True)
                 # Keep below for server
