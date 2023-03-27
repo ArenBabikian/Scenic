97e9b761d5a5e1f932eddabf715da1132859e6b9
ArenBabikian <aren.babikian@mail.mcgill.ca>
2023-03-27 16:58:42 -0400

    clean-up

--- scripts/runAttilaAll.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-bash runAttila.sh 2actors
-bash runAttila.sh 2actors-med
-bash runAttila.sh 3actors
-bash runAttila.sh 3actors-med
-bash runAttila.sh 4actors
-bash runAttila.sh 4actors-med
